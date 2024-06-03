5a8ad40f41f2b9c7647aabe04dff6e8b77a31572
Ayrton <ayrtton.vinicius@gmail.com>
2024-06-03 18:25:55 -0300

    Design the desktop homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import AppRoutes from './AppRoutes';
 import './assets/css/main.css';
 
 function App() {
 	return (
-		<h1>Hello World</h1>
+		<AppRoutes />
 	);
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+function Home() {
+    return (
+        <main className="home">
+            <div className="hero">
+                <div className="hero__content">
+                    <span className="hero__content-message">So, you want to travel to</span>
+                    <h1 className="hero__content-title">Space</h1>
+                    <p className="hero__content-text">
+                        Let’s face it; if you want to go to space, you might as well genuinely go to 
+                        outer space and not hover kind of on the edge of it. Well sit back, and relax 
+                        because we’ll give you a truly out of this world experience!
+                    </p>
+                </div>
+                <a className="hero__button" href="" target="_blank">Explore</a>
+            </div>
+        </main>
+    )
+}
+
+export default Home;
