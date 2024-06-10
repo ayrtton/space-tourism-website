5dc5f91be6a53b37a7a04031cf94c670c8b63fe7
Ayrton <ayrtton.vinicius@gmail.com>
2024-06-10 18:31:42 -0300

    Create the footer

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function AppRoutes() {
 
@@ -8,6 +9,7 @@ function AppRoutes() {
             <Routes>
                 <Route path="/" element={<Home />}></Route>
             </Routes>
+            <Footer />
         </Router>
     );
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer>
+            <div className="attribution">
+                <p className="attribution__source">Challenge by&nbsp;
+                    <a className="attribution__link" href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">
+                    Frontend Mentor</a>.&nbsp;
+                </p>
+                <p className="attribution__developer">Coded by&nbsp;
+                    <a className="attribution__link" href="https://github.com/ayrtton" target="_blank" rel="noreferrer">Ayrton</a>.
+                </p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
