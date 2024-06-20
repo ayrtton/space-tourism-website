bb104e4d9363f2b6b20fc57e2f72a35f4a1b4294
Ayrton <ayrtton.vinicius@gmail.com>
2024-06-20 17:55:14 -0300

    Build the mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,27 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/images/shared/logo.svg';
+import iconHamburger from '../assets/images/shared/icon-hamburger.svg';
+import iconClose from '../assets/images/shared/icon-close.svg';
 
 function Navbar() {
+    const [menu, setMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setMenu(!menu);
+    }
+
     return (
         <nav className="nav">
             <div className="logo">
                 <img className="logo__image" src={logo} alt="" />
             </div>
             <hr className="line" />
-            <ul className="menu">
-                <li className="menu__item">
+            <div className="icon">
+                <img className="icon__open" src={ menu === true ? iconClose : iconHamburger } alt="" onClick={toggleMenu} />
+            </div>
+            <ul className={`menu ${ menu === true ? "open" : "" }`}>
+                <li className="menu__item" onClick={toggleMenu}>
                     <NavLink className="menu__item-link" to="/" activeClassName="active">
                         <div className="menu__item-number">00</div>
                         <div className="menu__item-title">Home</div>
